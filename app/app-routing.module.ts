c971648952f3ba42c0047fa8ce4a44456b4bcc9f
riccardoscalet <riccardo.scalet@gmail.com>
2016-11-10 16:18:18 +0100

    Set the not-found route.

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -4,18 +4,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from "./login.component";
 
 const routes: Routes = [{
-        path: '',
-        redirectTo: '/login',
-        pathMatch: 'full'
+        path: "",
+        redirectTo: "login",
+        pathMatch: "full"
     },
     {
-        path: 'login',
+        path: "login",
         component: LoginComponent
     },
     // {
     //     path: 'detail/:id',
     //     component: HeroDetailComponent
     // }
+    {
+        // Not-found route, to redirect every wrong path (instead of causing an exception)
+        path: "**",
+        redirectTo: "login"
+    }
 ];
 
 @NgModule({
